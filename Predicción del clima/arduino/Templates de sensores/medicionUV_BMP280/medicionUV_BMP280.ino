3a11f54d4c79fae14a5e5e5f36e3d034044a3d0f
Manuel Castillo Obregón <manuelecastilloo@gmail.com>
2020-03-13 16:49:00 -0600

    Código de sensores para el Arduino.

---
+++ Predicción del clima/arduino/Templates de sensores/medicionUV_BMP280/medicionUV_BMP280.ino
@@ -0,0 +1,74 @@
+
+ 
+/*
+ * 
+***************************************************************************
+  This is a library for the BMP280 humidity, temperature & pressure sensor
+  Get library from: https://github.com/adafruit/Adafruit_BMP280_Library
+  This code has been downloaded from Robojax.com 
+  Watch video instruction of this code: https://youtu.be/InNHrtnZgUQ
+  Video Recorded Jane 22, 2019 in Ajax, Ontario Canada
+  by Ahmad Shamshiri
+
+  Designed specifically to work with the Adafruit BMEP280 Breakout
+  ----> http://www.adafruit.com/products/2651
+
+  These sensors use I2C or SPI to communicate, 2 or 4 pins are required
+  to interface.
+
+  Adafruit invests time and resources providing this open source code,
+  please support Adafruit andopen-source hardware by purchasing products
+  from Adafruit!
+
+  Written by Limor Fried & Kevin Townsend for Adafruit Industries.
+  BSD license, all text above must be included in any redistribution
+ ***************************************************************************/
+
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_BMP280.h>
+
+#define BMP_SCK  (13)
+#define BMP_MISO (12)
+#define BMP_MOSI (11)
+#define BMP_CS   (10)
+
+Adafruit_BMP280 bmp; // I2C
+//Adafruit_BMP280 bmp(BMP_CS); // hardware SPI
+//Adafruit_BMP280 bmp(BMP_CS, BMP_MOSI, BMP_MISO,  BMP_SCK);
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println(F("BMP280 test"));
+
+  if (!bmp.begin()) {
+    Serial.println(F("Could not find a valid BMP280 sensor, check wiring!"));
+    while (1);
+  }
+
+  /* Default settings from datasheet. */
+  bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
+                  Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
+                  Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
+                  Adafruit_BMP280::FILTER_X16,      /* Filtering. */
+                  Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
+}
+
+void loop() {
+    Serial.print(F("Temperature = "));
+    Serial.print(bmp.readTemperature());
+    Serial.println(" *C");
+    
+    Serial.print(F("Pressure = "));
+    Serial.print(bmp.readPressure());
+    Serial.println(" Pa");
+
+    Serial.print(F("Approx altitude = "));
+    Serial.print(bmp.readAltitude(1013.25)); /* Adjusted to local forecast! */
+    Serial.println(" m");
+
+
+
+    Serial.println();
+    delay(2000);
+}
