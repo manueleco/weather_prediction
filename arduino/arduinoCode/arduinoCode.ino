990b71434fe3197fd8df7ef2483d5bc87f9875dc
manupianista <manuelecastilloo@gmail.com>
2020-03-14 19:21:50 -0600

    depuraciones de codigo arduino

--- arduino/arduinoCode/arduinoCode.ino
+++ arduino/arduinoCode/arduinoCode.ino
@@ -7,44 +7,55 @@
 #include <SPI.h>
 #include <Adafruit_BMP280.h>
 
-#define DHTPIN 2     
+#include "SD.h"
+#include "SPI.h"
 
+#define DHTPIN 2     
 
 #define BMP_SCK  (13)
 #define BMP_MISO (12)
 #define BMP_MOSI (11)
 #define BMP_CS   (10)
 
+// ======================================================== BMP280 ==================================================
 Adafruit_BMP280 bmp; // I2C
 //Adafruit_BMP280 bmp(BMP_CS); // hardware SPI
 //Adafruit_BMP280 bmp(BMP_CS, BMP_MOSI, BMP_MISO,  BMP_SCK);
 
 
+// ======================================================== DHT11 ==================================================
 #define DHTTYPE DHT11   // DHT 11
-//#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
-//#define DHTTYPE DHT21   // DHT 21 (AM2301)
 
-// Connect pin 1 (on the left) of the sensor to +5V
-// NOTE: If using a board with 3.3V logic like an Arduino Due connect pin 1
-// to 3.3V instead of 5V!
-// Connect pin 2 of the sensor to whatever your DHTPIN is
-// Connect pin 4 (on the right) of the sensor to GROUND
-// Connect a 10K resistor from pin 2 (data) to pin 1 (power) of the sensor
 
-// Initialize DHT sensor.
-// Note that older versions of this library took an optional third parameter to
-// tweak the timings for faster processors.  This parameter is no longer needed
-// as the current DHT reading algorithm adjusts itself to work on faster procs.
+// pin 1 (izquierda del sensor) a +5V
+// pin 2 del sensor a cualquier DHTPIN
+// pin 4 (derecha del sensor) a GROUND
+// resistencia de 10k del pin 2 (data) a pin 1 (power) del sensor
+
+/ inicialización del sensor
 DHT dht(DHTPIN, DHTTYPE);
 
+
+// ======================================================== SD CARD ==================================================
+const int CSpin = 10;
+String dataString =""; // holds the data to be written to the SD card
+float sensorReading1 = 0.00; // value read from your first sensor
+float sensorReading2 = 0.00; // value read from your second sensor
+float sensorReading3 = 0.00; // value read from your third sensor
+File sensorData;
+
 void setup() {
+  
   Serial.begin(9600);
   Serial.println("Medicion de variables para el clima");
 
+   //inicializar dht
+   Serial.println("Inicializando DHT11");
   dht.begin();
 
+  Serial.println("Inicializando BMP280");
   if (!bmp.begin()) {
-    Serial.println(F("Could not find a valid BMP280 sensor, check wiring!"));
+    Serial.println(F("NO SE HA ENCONTRADO SENSOR BMP280"));
     while (1);
   }
 
@@ -54,45 +65,50 @@ void setup() {
                   Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
                   Adafruit_BMP280::FILTER_X16,      /* Filtering. */
                   Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
+
+  /*SETUP DE SD*/
+  Serial.println("Inicializando tarjeta SD");
+  pinMode(CSpin, OUTPUT);
 }
 
 void loop() {
-  // Wait a few seconds between measurements.
   
-
-
-  // Robojax.com test video
-  Serial.print("Temperature: ");
+  Serial.print("Temperatura de DHT11: ");
   
   Serial.print(getTemp("c"));
   
   Serial.print(" *C ");
   Serial.print(getTemp("f"));
   Serial.println (" *F");
+  
   Serial.println("-----------------");  
-  Serial.print("Heat index: ");
+  
+  Serial.print("Indice de calor: ");
   Serial.print(getTemp("hic"));
   Serial.print(" *C ");
   Serial.print(getTemp("hif"));
   Serial.println(" *F");
   Serial.print(getTemp("k"));
   Serial.println(" *K");
-  Serial.println("-----------------");    
-  Serial.print("Humidity: ");
+  
+  Serial.println("-----------------");  
+    
+  Serial.print("Humedad: ");
   Serial.print(getTemp("h"));
   Serial.println(" % ");
+  
   Serial.println("===========================");
 
-  Serial.print(F("Temperature = "));
+  Serial.print(F("Temperatura de BMP280 = "));
     Serial.print(bmp.readTemperature());
     Serial.println(" *C");
     
-    Serial.print(F("Pressure = "));
+    Serial.print(F("Presión = "));
     Serial.print(bmp.readPressure());
     Serial.println(" Pa");
 
-    Serial.print(F("Approx altitude = "));
-    Serial.print(bmp.readAltitude(1013.25)); /* Adjusted to local forecast! */
+    Serial.print(F("Altitud aprox = "));
+    Serial.print(bmp.readAltitude(1013.25)); /* AJUSTAR! */
     Serial.println(" m");
 
 
@@ -102,19 +118,7 @@ void loop() {
     
 }
 
-
-/*
- * getTemp(String req)
- * returns the temprature related parameters
- * req is string request
- * This code can display temprature in:
- * getTemp("c") is used to get Celsius
- * getTemp("f") is used to get fahrenheit
- * getTemp("k") is used for Kelvin
- * getTemp("hif") is used to get fahrenheit
- * getTemp("hic") is used to get Celsius
- * getTemp("f") is used to get humidity 
- */
+ 
 float getTemp(String req)
 {
 
@@ -155,3 +159,20 @@ float getTemp(String req)
   }
  
 }
+
+
+
+// ======================================================== SIDE NOTES ==================================================
+
+/*
+ * getTemp(String req)
+ * returns the temprature related parameters
+ * req is string request
+ * This code can display temprature in:
+ * getTemp("c") is used to get Celsius
+ * getTemp("f") is used to get fahrenheit
+ * getTemp("k") is used for Kelvin
+ * getTemp("hif") is used to get fahrenheit
+ * getTemp("hic") is used to get Celsius
+ * getTemp("f") is used to get humidity 
+ */
