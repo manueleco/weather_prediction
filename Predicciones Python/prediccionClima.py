469a1107590620ee5c6f80f836664648acc28930
manupianista <manuelecastilloo@gmail.com>
2020-03-17 14:10:40 -0600

    merge dev1

--- Predicciones Python/prediccionClima.py
+++ Predicciones Python/prediccionClima.py
@@ -5,14 +5,11 @@ import time
 
 states = ["Soleado","Nublado"]
 
-# Possible sequences of events
 transitionName = [["SuSu","SuCl"],["ClCl","ClSu"]]
 
-# Probabilities matrix (transition matrix)
 transitionMatrix = [[0.8,0.2],[0.4,0.6]]
 
 
-# Check that probabilities add to 1. If not, raise ValueError
 if sum(transitionMatrix[0])+sum(transitionMatrix[1]) != 2:
     print("Error!")
     raise ValueError("Probabilities MUST ADD TO 1")
@@ -26,7 +23,6 @@ def weatherForecast(days):
     print("Starting weather: ",weatherToday)
     while i &lt; days:
         if weatherToday == "Soleado":
-            #numpy.random.choice(a, size=None, replace=True, p=None)
             change = np.random.choice(transitionName[0],replace=True,p=transitionMatrix[0])
             if change == "SuSu":
                 pass
